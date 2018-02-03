628f187cfa94ec7ad6cf624ddd3644ee44614f65
Matteson Grossman <mattesongrossman@Mattesons-MacBook-Pro.local>
2018-02-03 11:00:47 -0500

    Added basic functionality including if statement with alert for cards

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,6 +1,26 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log(cardOne);
+
+var cards = ["queen","queen","king","king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2){
+	if(cardOne === cardTwo){
+		alert("You found a match");
+	}
+	else{
+		alert("Sorry Try Again");
+	}
+}
