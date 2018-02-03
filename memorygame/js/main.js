85f98a2137f3fb2eebf8ba53a950e7f9fe9f0aea
Matteson Grossman <mattesongrossman@Mattesons-MacBook-Pro.local>
2018-02-03 12:32:28 -0500

    added function for flipCard

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -4,23 +4,23 @@ var cards = ["queen","queen","king","king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2){
-	if(cardOne === cardTwo){
-		alert("You found a match");
-	}
-	else{
-		alert("Sorry Try Again");
-	}
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
 }
+}
+
+var flipCard = function(cardId){
+	console.log(cards[cardId] + " has been flipped");
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2){
+	checkForMatch();}
+}
+flipCard(0);
+flipCard(2);
+
+
+
+
